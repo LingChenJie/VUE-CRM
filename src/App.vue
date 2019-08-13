5ae51fe1ceefc2577c8874faaeaf9e45f1498142
lingchenjie <976918915@qq.com>
2019-08-13 16:13:50 +0800

    修改了基础配置

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
 <template>
-    <div>这是App组件</div>
+    <div class="app-container">
+        <!-- 顶部 Header 区域 -->
+        <mt-header fixed title="hello Vue"></mt-header>
+
+        <h1>中间区域</h1>
+        <h1>中间区域</h1>
+
+        <!-- 底部 Tabbar 区域 -->
+        <nav class="mui-bar mui-bar-tab">
+            <a class="mui-tab-item" href="#tabbar">
+                <span class="mui-icon mui-icon-home"></span>
+                <span class="mui-tab-label">首页</span>
+            </a>
+            <a class="mui-tab-item mui-active" href="#tabbar-with-chat">
+                <span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+                <span class="mui-tab-label">消息</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-contact">
+                <span class="mui-icon mui-icon-contact"></span>
+                <span class="mui-tab-label">通讯录</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-map">
+                <span class="mui-icon mui-icon-gear"></span>
+                <span class="mui-tab-label">设置</span>
+            </a>
+        </nav>
+    </div>
+    
     
 </template>
 
@@ -16,7 +43,10 @@ export default {
 };
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+    .app-container {
+        padding-top: 40px;
+        overflow: hidden;
+    }
 </style>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,22 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
+// 导入 vue-resource
+import VueResource from 'vue-resource'
+// 安装 vue-resource
+Vue.use(VueResource)
+
+// 导入 mint-ui 相关组件
+import { Header } from 'mint-ui'
+Vue.component(Header.name, Header)
+// 导入 mint-ui 样式
+import 'mint-ui/lib/style.css'
+
+// 导入 MUI 的样式
+import './lib/mui/css/mui.min.css'
+// 导入扩展图标样式
+//import './lib/mui/css/icons-extra.css'
+
 // 导入路由模块
 import router from './router.js'
 
